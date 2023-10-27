15fb4c17ef767a24702a22082c53c56efb5f152a
Shriharsh <shriharsh.dev@gmail.com>
2023-10-27 14:38:52 +0530

    Added some packages and modified app.js

--- backend/express-locallibrary-tutorial/app.js
+++ backend/express-locallibrary-tutorial/app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require("compression");
+const helmet = require('helmet')
+const RateLimit = require('express-rate-limit')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -10,10 +13,20 @@ const coolRouter = require('./routes/cool')
 const catalogRouter = require('./routes/catalog')
 
 const app = express();
+app.use(helmet.contentSecurityPolicy({
+  directives:{
+    "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+  }
+}))
+const limiter = RateLimit({
+  windowMs:1*60*1000,
+  max:20,
+})
+app.use(limiter)
 const mongoose = require('mongoose')
 mongoose.set("strictQuery",false)
-const mongoDB = "mongodb+srv://shriharshdev:lLG3bCti3nWk7adS@cluster0.ynhzz9a.mongodb.net/local_library?retryWrites=true&w=majority"
-
+const dev_db_url = "mongodb+srv://shriharshdev:lLG3bCti3nWk7adS@cluster0.ynhzz9a.mongodb.net/local_library?retryWrites=true&w=majority"
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
@@ -27,6 +40,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
